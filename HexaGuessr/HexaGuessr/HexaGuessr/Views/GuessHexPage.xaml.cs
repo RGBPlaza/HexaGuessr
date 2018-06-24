7a233a7b6de9eddc929d6ccfa6054c00f75c495e
Joshua Yarrow <rayred14@live.com>
2018-06-24 22:19:09 +0100

    New File

---
+++ HexaGuessr/HexaGuessr/HexaGuessr/Views/GuessHexPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HexaGuessr.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GuessHexPage : ContentPage
+	{
+		public GuessHexPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
